02c2e23587a2f234997805bcc37521f13b257d89
vsonhuang <vson.mail@gmail.com>
2016-03-18 17:06:22 +0800

    vson

---
+++ Applications/Core/Evisou.Core.Cache/ICacheProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evisou.Core.Cache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICacheProvider
+    {
+        object Get(string key);
+        void Set(string key, object value, int minutes, bool isAbsoluteExpiration, Action<string, object, string> onRemove);
+        void Remove(string key);
+        void Clear(string keyRegex);
+    }
+}
---
+++ Applications/Core/Evisou.Core.Log/LogDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Evisou.Core.Config;
+using Evisou.Framework.Contract;
+using Evisou.Framework.DAL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evisou.Core.Log
+{
+    [Table("AuditLog")]
+    public class AuditLog : ModelBase
+    {
+        public int ModelId { get; set; }
+        public string UserName { get; set; }
+        public string ModuleName { get; set; }
+        public string TableName { get; set; }
+        public string EventType { get; set; }
+        public string NewValues { get; set; }
+    }
+
+    public class LogDbContext : DbContextBase, IAuditable
+    {
+        public LogDbContext()
+            : base(CachedConfigContext.Current.DaoConfig.Log)
+          //  : base("Data Source=(LocalDb)\v11.0;AttachDbFilename=|DataDirectory|\aspnet-WebApplication2-20150222121646.mdf;Initial Catalog=aspnet-WebApplication2-20150222121646;Integrated Security=True")
+        {
+            Database.SetInitializer<LogDbContext>(null);
+        }
+
+        public DbSet<AuditLog> AuditLogs { get; set; }
+
+        public void WriteLog(int modelId, string userName, string moduleName, string tableName, string eventType, ModelBase newValues)
+        {
+            this.AuditLogs.Add(new AuditLog()
+            {
+                ModelId = modelId,
+                UserName = userName,
+                ModuleName = moduleName,
+                TableName = tableName,
+                EventType = eventType,
+                NewValues = JsonConvert.SerializeObject(newValues, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore })
+            });
+
+            this.SaveChanges();
+            this.Dispose();
+        }
+    }
+}
